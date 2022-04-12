cb08d5d883d10fca26e6393d8e1b704a88cdf0cc
Pradeep <ps178@snu.edu.in>
2022-04-12 18:52:49 +0530

    fixed unique user bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ public function register (Request $request) {
     
 public function login (Request $request) {
     $request->validate([
-        'email' => 'required|string|email|max:255|unique:users',
+        'email' => 'required|string|email|max:255',
         'password' => 'required|string|min:6',
     ]);
     $user = User::where('email', $request->email)->first();
