f09ad6ee5fe3bc7e750ca4661b7ea8a5d094f6f3
Pradeep <ps178@snu.edu.in>
2022-04-12 17:57:36 +0530

    shops

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Shops;
+use App\Models\Shops;
 
 /*
 |--------------------------------------------------------------------------
